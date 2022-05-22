4a525cf8b4d60eba1e69f6820aef5daca930632b
BOC Dev <53903568+Qualia91@users.noreply.github.com>
2022-05-22 12:37:08 +0100

    first

---
+++ README.md
@@ -0,0 +1,21 @@
+Overview
+============
+
+This is a Logtail backend for logtail which lets you send lager logs to your logtail account.
+
+##Configuration
+Configure a Lager handler like the following:
+
+	{lager_logtail_backend, [Level, MaxRetries, RetryInterval, LogtailToken]}
+
+* Level - The lager level at which the  backend accepts messages (eg. using ‘info’ would send all messages at info level or above into syslog)
+* MaxRetries - The maximum number of retries the backend will do before giving up on Logtail
+* RetryInterval - The interval at which each retry is performed. i.e. Retries 5 and Interval 3 means that it will try a maximum of 5 times with 3 seconds apart
+* LogtailToken - This is your unique Logtail token
+
+
+An example might look something like this:
+
+	{lager_logtail_backend, [info, 5, 3, <OWN_TOKEN>]}
+
+Refer to Lager’s documentation for further information on configuring handlers.
---
+++ src/lager_logtail_backend.erl
@@ -0,0 +1,124 @@
+%% ----------------------------------------------------------------------------
+%%
+%% lager_logtail: Logtail backend for Lager
+%%
+%% Copyright (c) 2012 KIVRA
+%%
+%% Permission is hereby granted, free of charge, to any person obtaining a
+%% copy of this software and associated documentation files (the "Software"),
+%% to deal in the Software without restriction, including without limitation
+%% the rights to use, copy, modify, merge, publish, distribute, sublicense,
+%% and/or sell copies of the Software, and to permit persons to whom the
+%% Software is furnished to do so, subject to the following conditions:
+%%
+%% The above copyright notice and this permission notice shall be included in
+%% all copies or substantial portions of the Software.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+%% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+%% DEALINGS IN THE SOFTWARE.
+%%
+%% ----------------------------------------------------------------------------
+
+-module(lager_logtail_backend).
+
+-behaviour(gen_event).
+
+-export([
+         init/1,
+         handle_call/2,
+         handle_event/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3
+        ]).
+
+-type lager_msg_metadata() :: [tuple()].
+-type binary_proplist() :: [{binary(), binary()}].
+
+%%% this is only exported for the spawn call
+-export([deferred_log/3]).
+
+-record(state, {
+                 level          :: integer(),
+                 retry_interval :: integer(),
+                 retry_times    :: integer(),
+                 token          :: string(),
+                 url            :: string() = "https://in.logtail.com"
+               }).
+
+-include_lib("lager/include/lager.hrl").
+
+init([Level, RetryTimes, RetryInterval, Token]) ->
+    State = #state{
+                    level          = lager_util:level_to_num(Level),
+                    retry_interval = RetryInterval,
+                    retry_times    = RetryTimes,
+                    token          = Token
+                  },
+    {ok, State}.
+
+handle_call(get_loglevel, #state{ level = Level } = State) ->
+    {ok, Level, State};
+handle_call({set_loglevel, Level}, State) ->
+    {ok, ok, State#state{ level = lager_util:level_to_num(Level) }};
+handle_call(_Request, State) ->
+    {ok, ok, State}.
+
+%% @private
+handle_event({log, Message}, #state{level=Level} = State) ->
+    case lager_util:is_loggable(Message, Level, ?MODULE) of
+        true ->
+            Payload = jsx:encode(cons_metadata_to_binary_proplist(lager_msg:metadata(Message), [
+                                     {<<"level">>, any_to_binary(lager_msg:severity(Message))}
+                                    ,{<<"message">>, any_to_binary(lager_msg:message(Message))}
+                                 ])),
+            Request = {State#state.url, [{"te", "chunked"}], "Content-Type: application/json", "Authorization: Bearer " ++ State#state.token, Payload},
+            RetryTimes = State#state.retry_times,
+            RetryInterval = State#state.retry_interval,
+
+            %% Spawn a background process to handle sending the payload.
+            %% It will recurse until the payload has ben successfully sent.
+            spawn(?MODULE, deferred_log, [Request, RetryTimes, RetryInterval]),
+            {ok, State};
+        false ->
+            {ok, State}
+    end;
+handle_event(_Event, State) ->
+    {ok, State}.
+
+handle_info(_Info, State) ->
+    {ok, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%% Private
+
+deferred_log(_Request, 0, _) ->
+    ok;
+deferred_log(Request, Retries, Interval) ->
+    case httpc:request(post, Request, [], [{body_format, binary}]) of
+        {ok, {{_, 200, _}, _H, _B}} -> ok;
+        _ ->
+            timer:sleep(Interval * 1000),
+            deferred_log(Request, Retries - 1, Interval)
+    end.
+
+-spec cons_metadata_to_binary_proplist(Metadata::lager_msg_metadata(), Proplist::binary_proplist()) -> Proplist::binary_proplist().
+cons_metadata_to_binary_proplist(Metadata, Proplist) ->
+    lists:foldl(fun({Key, Value}, Acc) -> [{any_to_binary(Key), any_to_binary(Value)} | Acc] end, Proplist, Metadata).
+
+any_to_binary(V) when is_atom(V)    -> any_to_binary(atom_to_list(V));
+any_to_binary(V) when is_pid(V)     -> any_to_binary(pid_to_list(V));
+any_to_binary(V) when is_list(V)    -> list_to_binary(V);
+any_to_binary(V) when is_integer(V) -> integer_to_binary(V);
+any_to_binary(V) when is_binary(V)  -> V.
+
